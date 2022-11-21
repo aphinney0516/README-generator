f4141fdd14860f1ad1e244a1cec78bc11c360064
Andy Phinney <111609415+aphinney0516@users.noreply.github.com>
2022-11-21 13:27:26 -0600

    Completed initial set up and made an array with all of the questions a good README will answer

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What was your motivation to build this particular project?', 
+    'Why did you build this project?', 
+    'What problem does it solve?', 
+    'What did you learn?', 
+    'What makes your project stand out?', 
+    'What challenges did you face?', 
+    'What features do you plan to implement in the future?',
+    'What are the steps required to install your project?',
+    'What are some examples regarding how this project will be used?',
+    'What collaborators, third-party assets, or tutorials were used to build this project? (if any)',
+    'What features does your project have available?',
+    'What are the instructions for contributions?'
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
