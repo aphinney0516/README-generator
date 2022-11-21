c87de9110e95abc9baaa767e4d3e297a90205d12
Andy Phinney <111609415+aphinney0516@users.noreply.github.com>
2022-11-21 15:28:30 -0600

    Installed inquirer and updated questions to fit in the inquirer prompt module

--- index.js
+++ index.js
@@ -1,26 +1,113 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const { message } = require('statuses');
 
-// TODO: Create an array of questions for user input
-const questions = [
-    'What was your motivation to build this particular project?', 
-    'Why did you build this project?', 
-    'What problem does it solve?', 
-    'What did you learn?', 
-    'What makes your project stand out?', 
-    'What challenges did you face?', 
-    'What features do you plan to implement in the future?',
-    'What are the steps required to install your project?',
-    'What are some examples regarding how this project will be used?',
-    'What collaborators, third-party assets, or tutorials were used to build this project? (if any)',
-    'What features does your project have available?',
-    'What are the instructions for contributions?'
-];
+// User prompts for README creation
+inquirer.createPromptModule(
+    [
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message: 'What was your motivation to build this particular project?', 
+            name: 'description',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message: 'Why did you build this project?',  
+            name: 'description',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message: 'What problem does it solve?',  
+            name: 'description',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message: 'What did you learn?',  
+            name: 'description',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message: 'What makes your project stand out?',  
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What challenges did you face?', 
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project?',
+            name: 'installation',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message: 'How do you use this project?',
+            name: 'usage',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message: 'What collaborators, third-party assets, or tutorials were used to build this project? (if any)',
+            name: 'credits',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message: 'What features does your project contain?',
+            name: 'features',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message:  'What features do you plan to implement in the future?',
+            name: 'future development',
+            validate: (value)=>{ if(value){return true} else {return 'Input required'}},
+        },
+        {
+            type: 'input',
+            message: 'What are the instructions for contributions?',
+            name: 'contributions',
+        },
+    ]
+)
+
+// const questions = [
+//     'What is the title of your project?',
+//     'What was your motivation to build this particular project?', 
+//     'Why did you build this project?', 
+//     'What problem does it solve?', 
+//     'What did you learn?', 
+//     'What makes your project stand out?', 
+//     'What challenges did you face?', 
+//     'What are the steps required to install your project?',
+//     'How do you use this project?',
+//     'What collaborators, third-party assets, or tutorials were used to build this project? (if any)',
+//     'What features does your project contain?',
+//     'What features do you plan to implement in the future?',
+//     'What are the instructions for contributions?'
+// ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
